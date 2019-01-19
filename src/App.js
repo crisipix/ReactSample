5d6214f772175eff1167f5fb6187b9869d4f33c3
crisipix <wongc5@gmail.com>
2019-01-18 23:21:52 -0500

    added 2 components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import React, { Component} from "react";
 //  as hot-reloaded by modifying to code as follows.
 import {hot} from "react-hot-loader";
 import "./App.css";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 class App extends Component{
+    // must call super on first line
+    constructor(){
+        super()
+        this.cName = "Will";
+    }
+    getVal()
+    {
+        return "Mike!";
+    }
+
   render(){
+    const name = 'Mike';
     return(
       <div className="App">
-        <h1> Hello, World! This is a test </h1>
+      <Header></Header>
+        <h1> Hello, World! This is a test {this.getVal()}</h1>
+        <h1> Constructor Property {this.cName}</h1>
+      <Footer></Footer>
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Tell our React app where to hook into the DOM (in our index.html)
 // ReactDOM.render is the function that tells React what to render
 //  and where to render it — component called App
+// It'll find the root element in index.html and render app there. 
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
